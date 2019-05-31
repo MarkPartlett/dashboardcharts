7b040d1551bad24a7fa697c8d309705a87bc99e4
Mark Partlett <mark@partlett.net>
2019-06-01 08:08:24 +1000

    First Commit to dashboardcharts

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -9,7 +9,6 @@
  */
 return [
     'routes' => [
-	   ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-	   ['name' => 'page#do_echo', 'url' => '/echo', 'verb' => 'POST'],
+	   ['name' => 'Settings#personal', 'url' => '/settings', 'verb' => 'POST']
     ]
 ];
